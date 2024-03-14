03df946d9a4e6c74b81100c1c2a58c9d5d519155
bassam207 <bassamhassan171@gmail.com>
2024-03-13 19:34:08 -0700

    py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,5 +15,5 @@ if last_d > 5 :
     print("and is greater than 5")
 elif last_d == 0 :
     print ("and is 0")
-elif last_d < 6 and last_d != 0 :
+else :
     print ("and is less than 6 and not 0")
