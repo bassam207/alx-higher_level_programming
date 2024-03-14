17c8a54580f854248cbc92df9c13b8ece9fd1788
bassam207 <bassamhassan171@gmail.com>
2024-03-14 12:41:42 -0700

    py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0,100):
+for n in range(0, 100):
     if n != 99:
-         print("{:d}".format(n).zfill(2),end =', ')
+        print("{:d}".format(n).zfill(2), end =', ')
     else:
         print("{:d}".format(n).zfill(2))
