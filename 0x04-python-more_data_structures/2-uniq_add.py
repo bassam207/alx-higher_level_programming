7c678ad8a639026cb7f7b4e4c329286fdbb5c434
bassam207 <bassamhassan171@gmail.com>
2024-04-20 11:39:11 -0700

    py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = set(my_list)
+    new_list = list(new_list)
+    result = 0
+    for num in new_list:
+        result += num
+    return (result)
