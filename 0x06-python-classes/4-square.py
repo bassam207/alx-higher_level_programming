e80abe5932b4fbe4febee70e85d99e62306504eb
bassam207 <bassamhassan191@gmail.com>
2024-05-13 12:21:03 -0700

    py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """Defines a class Square."""
 
-class square:
+
+class Square:
     """Represents a square."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initializes a new square.
 
         Args:
-        size (int): The size of the new square."""
-
+            size (int): The size of the new square.
+        """
         self.__size = size
 
-        """ make getter method for size attribute"""
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @property
-        def size(self):
-            return (self.__size)
-        """ make setter method for size attribute"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
-        """ make area method."""
-
-        def area(self):
-            return (self.__size **2)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
