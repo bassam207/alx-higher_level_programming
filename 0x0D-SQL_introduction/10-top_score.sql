fa201d988248a46c5a290b541610c12147c5a6dd
bassam207 <bassamhassan191@gmail.com>
2024-05-22 12:18:11 -0700

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 --  script that lists all records of the table second_table and order them
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'SCORE' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `SCORE` DESC;
